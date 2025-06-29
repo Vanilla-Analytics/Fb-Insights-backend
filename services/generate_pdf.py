e828f305767e262ffbd44b98c6181b1e92ce236a
Shahin2512 <shahinp2512@gmail.com>
2025-06-30 02:31:26 +0530

    update

--- services/generate_pdf.py
+++ services/generate_pdf.py
@@ -454,7 +454,7 @@ def generate_pdf_report(sections: list, ad_insights_df=None,full_ad_insights_df=
                         # Draw Split Charts below the table
                         if 'split_charts' in locals() and split_charts and len(split_charts) >= 3:
                             #chart_y = table_y - performance_table._height - 10  # Start charts below table
-                            chart_y = BOTTOM_MARGIN + 80
+                            chart_y = BOTTOM_MARGIN + 50
             
                         # First two charts (donuts) side by side
                             chart_width = 220
@@ -478,7 +478,7 @@ def generate_pdf_report(sections: list, ad_insights_df=None,full_ad_insights_df=
                             # ROAS Chart (horizontal bar)
                             if len(split_charts) > 2:
                                 img3 = ImageReader(split_charts[2][1])
-                                roas_width = chart_width + 40
+                                roas_width = chart_width + 60
                                 c.drawImage(img3, x3, chart_y, width=roas_width, height=chart_height)
 
 
