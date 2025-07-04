9aa26ff7ee88d14bc7be3ca65eee6dade83ef332
Shahin2512 <shahinp2512@gmail.com>
2025-07-04 17:57:19 +0530

    moving table down

--- services/generate_pdf.py
+++ services/generate_pdf.py
@@ -378,7 +378,7 @@ def generate_pdf_report(sections: list, ad_insights_df=None,full_ad_insights_df=
 
                         
                         #table_y = LOGO_Y_OFFSET - LOGO_HEIGHT - 20  
-                        table_y = PAGE_HEIGHT - 350  # You can adjust this to 400 if still too high
+                        table_y = PAGE_HEIGHT - 500  # You can adjust this to 400 if still too high
                         summary_table.wrapOn(c, PAGE_WIDTH, PAGE_HEIGHT)
                         summary_table.drawOn(c, LEFT_MARGIN, table_y)
 
